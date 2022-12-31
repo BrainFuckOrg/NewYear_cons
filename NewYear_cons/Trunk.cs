2694077f3df14efb9bc8d322b35a23b4f61d9a42
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-31 22:41:21 +0200

    Trunk complete

--- NewYear_cons/Trunk.cs
+++ NewYear_cons/Trunk.cs
@@ -2,5 +2,20 @@ namespace NewYear_cons;
 
 public class Trunk
 {
-    
+    public static void printTrunk(Int32 height)
+    {
+        String trunk = "BFO";
+        Int32 center = Console.WindowWidth / 2;
+        for (Int32 j = 0; j < height; j++)
+        {
+            for (Int32 i = 0; i < center; i++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(trunk);
+        }
+
+    }
 }
