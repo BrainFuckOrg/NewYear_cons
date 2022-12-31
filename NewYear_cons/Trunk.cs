f352c3f6092c5987d55731c5da5d4325b480b687
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-31 22:51:00 +0200

    Wishes

--- NewYear_cons/Trunk.cs
+++ NewYear_cons/Trunk.cs
@@ -2,7 +2,7 @@ namespace NewYear_cons;
 
 public class Trunk
 {
-    public static void printTrunk(Int32 height)
+    public static void PrintTrunk(Int32 height)
     {
         String trunk = "BFO";
         Int32 center = Console.WindowWidth / 2;
@@ -15,7 +15,26 @@ public class Trunk
 
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine(trunk);
+            Console.ResetColor();
         }
 
+    }
+
+    public static void BfOwish()
+    {
+        String[] wishes = new String[]
+        {
+            "Brain Fuck organisation wishes you to:", "write code like crazy","take part in the inhuman experiments","and have fun with coding"
+        };
+        Console.ForegroundColor = ConsoleColor.Green;
+        Int32 center = Console.WindowWidth / 2;
+        foreach (var wish in wishes)
+        {
+            for (Int32 i = 0; i < center; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(wish);
+        }
     }
 }
